2afe29c873dd9247ff2bbe419daa2cc03770d344
Glen Valencius <glen.valencius@gmail.com>
2025-05-05 20:48:18 +0700

    Update 03 Test Your Skill.py

--- Streamlit/pages/03 Test Your Skill.py
+++ Streamlit/pages/03 Test Your Skill.py
@@ -173,14 +173,14 @@ player_badge_count = balanced_df['Number of Gym Badges'].iloc[st.session_state.i
 num_badges = min(player_badge_count, 8)  # cap at 8 badges
 
 link_badge = [
-    'https://archives.bulbagarden.net/media/upload/thumb/d/dd/Boulder_Badge.png/1200px-Boulder_Badge.png',
-    'https://archives.bulbagarden.net/media/upload/9/9c/Cascade_Badge.png',
-    'https://archives.bulbagarden.net/media/upload/a/a6/Thunder_Badge.png',
-    'https://archives.bulbagarden.net/media/upload/b/b5/Rainbow_Badge.png',
-    'https://archives.bulbagarden.net/media/upload/7/7d/Soul_Badge.png',
-    'https://archives.bulbagarden.net/media/upload/6/6b/Marsh_Badge.png',
-    'https://archives.bulbagarden.net/media/upload/1/12/Volcano_Badge.png',
-    'https://archives.bulbagarden.net/media/upload/7/78/Earth_Badge.png'
+    'Streamlit/image/badges/badge_1.png',
+    'Streamlit/image/badges/badge_2.png',
+    'Streamlit/image/badges/badge_3.png',
+    'Streamlit/image/badges/badge_4.png',
+    'Streamlit/image/badges/badge_5.png',
+    'Streamlit/image/badges/badge_6.png',
+    'Streamlit/image/badges/badge_7.png',
+    'Streamlit/image/badges/badge_8.png'
 ]
 
 badges_html = ''.join([
